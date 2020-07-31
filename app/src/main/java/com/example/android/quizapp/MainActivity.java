0314100941f7b602d17b8fbc827ac525885afe1a
thech999 <thech999@gmail.com>
2020-07-31 21:55:55 +0200

    Quiz app 2.0

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.android.quizapp;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean answered = false;
+
+    float result = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,70 +20,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void resultButton(View view) {
+    public void resultButton() {
 
-        int points = 0;
+        EditText nameField = findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
 
-        boolean answer1 = ((RadioButton) view).isChecked();
+        if (!answered) {
 
-        switch (view.getId()) {
-            case R.id.answer1_1:
-                if (answer1)
-                    points += 1;
-                break;
-            case R.id.answer1_2:
-                if (answer1)
-                    break;
-            case R.id.answer1_3:
-                if (answer1)
-                    break;
-        }
+            if (((RadioButton) findViewById(R.id.answer1_1)).isChecked()) {
+                result++;
+            }
 
-        CheckBox answerCheckBox2_1 = (CheckBox) findViewById(R.id.answer2_1);
-        boolean answer2_1 = answerCheckBox2_1.isChecked();
-        if (answer2_1) {
-            points += 1;
-        }
-        CheckBox answerCheckBox2_2 = (CheckBox) findViewById(R.id.answer2_2);
-        boolean answer2_2 = answerCheckBox2_2.isChecked();
-        if (answer2_2) {
-            points += 1;
-        }
-        CheckBox answerCheckBox2_3 = (CheckBox) findViewById(R.id.answer2_3);
-        answerCheckBox2_3.isChecked();
-        CheckBox answerCheckBox2_4 = (CheckBox) findViewById(R.id.answer2_4);
-        answerCheckBox2_4.isChecked();
+            if (((CheckBox) findViewById(R.id.answer2_1)).isChecked()) {
+                result = result + 0.5f;
+            }
+            if (((CheckBox) findViewById(R.id.answer2_2)).isChecked()) {
+                result = result + 0.5f;
+            }
+            if (((CheckBox) findViewById(R.id.answer3_2)).isChecked()) {
+                result = result + 0.25f;
+            }
+            if (((CheckBox) findViewById(R.id.answer3_3)).isChecked()) {
+                result = result + 0.25f;
+            }
+            if (((CheckBox) findViewById(R.id.answer3_4)).isChecked()) {
+                result = result + 0.25f;
+            }
+            if (((CheckBox) findViewById(R.id.answer3_5)).isChecked()) {
+                result = result + 0.25f;
+            }
 
-        CheckBox answerCheckBox3_1 = (CheckBox) findViewById(R.id.answer3_1);
-        answerCheckBox3_1.isChecked();
-        CheckBox answerCheckBox3_2 = (CheckBox) findViewById(R.id.answer3_2);
-        boolean answer3_2 = answerCheckBox3_2.isChecked();
-        if (answer3_2) {
-            points += 1;
-        }
-        CheckBox answerCheckBox3_3 = (CheckBox) findViewById(R.id.answer3_3);
-        boolean answer3_3 = answerCheckBox3_3.isChecked();
-        if (answer3_3) {
-            points += 1;
-        }
-        CheckBox answerCheckBox3_4 = (CheckBox) findViewById(R.id.answer3_4);
-        boolean answer3_4 = answerCheckBox3_4.isChecked();
-        if (answer3_4) {
-            points += 1;
-        }
-        CheckBox answerCheckBox3_5 = (CheckBox) findViewById(R.id.answer3_5);
-        boolean answer3_5 = answerCheckBox3_5.isChecked();
-        if (answer3_5) {
-            points += 1;
+            answered = true;
+
+        } else {
+            //TODO: Result button clicked more than once
         }
 
-        displayMark(points);
+        DialogFragment newFragment = new MarkTextDialogFragment();
+        newFragment.show(getSupportFragmentManager(), totalMark(result, name));
 
     }
 
-    private void displayMark(int total) {
-        TextView scoreTextView = (TextView) findViewById(R.id.score);
-        scoreTextView.setText("" + total);
+
+    private String totalMark(float mark, String name) {
+
+        if (mark <= 2) {
+            return name + "\nYou fail the test.\nTry Again!";
+        }
+
+        if (mark < 4) {
+            return name + "\nCongratulations!\nYou pass!";
+        }
+
+        if (mark == 4) {
+            return name + "\nYou´re amazing!\nYou pass with a 10!";
+        }
+        return name;
     }
 
+    private static class MarkTextDialogFragment extends DialogFragment {
+    }
 }
