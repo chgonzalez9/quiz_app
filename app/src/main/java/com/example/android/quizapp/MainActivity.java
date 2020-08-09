f0179e5b054223e72c592ff3507285e883819915
thech999 <thech999@gmail.com>
2020-08-09 18:53:44 +0200

    Quiz App new version.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -13,16 +13,90 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    RadioButton answer1_1, answer1_2, answer1_3;
+
+    CheckBox answer2_1, answer2_2, answer2_3, answer2_4, answer3_1, answer3_2, answer3_3, answer3_4, answer3_5;
+
     boolean answered = false;
 
     float result = 0;
 
-    String Q5S = "1954";
+    String Q4S = "1954";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        answer1_1 = findViewById(R.id.answer1_1);
+        answer1_2 = findViewById(R.id.answer1_2);
+        answer1_3 = findViewById(R.id.answer1_3);
+        answer2_1 = findViewById(R.id.answer2_1);
+        answer2_2 = findViewById(R.id.answer2_2);
+        answer2_3 = findViewById(R.id.answer2_3);
+        answer2_4 = findViewById(R.id.answer2_4);
+        answer3_1 = findViewById(R.id.answer3_1);
+        answer3_2 = findViewById(R.id.answer3_2);
+        answer3_3 = findViewById(R.id.answer3_3);
+        answer3_4 = findViewById(R.id.answer3_4);
+        answer3_5 = findViewById(R.id.answer3_5);
+    }
+
+    public void radioButtonAnswer(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        String str = "";
+
+        switch (view.getId()) {
+            case R.id.answer1_1:
+                if (checked)
+                    str = "Answer A Selected";
+                break;
+            case R.id.answer1_2:
+                if (checked)
+                    str = "Answer B Selected";
+                break;
+            case R.id.answer1_3:
+                if (checked)
+                    str = "Answer C Selected";
+                break;
+        }
+        Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
+    }
+
+    public void checkBoxAnswer(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        String str = "";
+        // Check which checkbox was clicked
+        switch (view.getId()) {
+            case R.id.answer2_1:
+                str = checked ? "Thermodynamics Selected" : "Thermodynamics Deselected";
+                break;
+            case R.id.answer2_2:
+                str = checked ? "Quantum mechanics Selected" : "Quantum mechanics Deselected";
+                break;
+            case R.id.answer2_3:
+                str = checked ? "Scientology Selected" : "Scientology Deselected";
+                break;
+            case R.id.answer2_4:
+                str = checked ? "Astronomy Selected" : "Astronomy Deselected";
+                break;
+            case R.id.answer3_1:
+                str = checked ? "China Selected" : "China Deselected";
+                break;
+            case R.id.answer3_2:
+                str = checked ? "Spain Selected" : "Spain Deselected";
+                break;
+            case R.id.answer3_3:
+                str = checked ? "Italy Selected" : "Italy Deselected";
+                break;
+            case R.id.answer3_4:
+                str = checked ? "Belgium Selected" : "Belgium Deselected";
+                break;
+            case R.id.answer3_5:
+                str = checked ? "Germany Selected" : "Germany Deselected";
+                break;
+        }
+        Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
     }
 
     public void resultButton(View view) {
@@ -30,61 +104,75 @@ public class MainActivity extends AppCompatActivity {
         EditText nameField = findViewById(R.id.name_field);
         String name = nameField.getText().toString();
 
+        String answerResult = "Quiz Answers \nName: " + name;
+
         if (!answered) {
 
-            if (((RadioButton) findViewById(R.id.answer1_1)).isChecked()) {
+            if (answer1_1.isChecked()) {
                 result++;
+                answerResult += "\nAnswer 1-A";
             }
 
-            if (((RadioButton) findViewById(R.id.answer1_2)).isChecked()) {
+            if (answer1_2.isChecked()) {
                 result--;
+                answerResult += "\nAnswer 1-B";
             }
 
-            if (((RadioButton) findViewById(R.id.answer1_3)).isChecked()) {
+            if (answer1_3.isChecked()) {
                 result--;
+                answerResult += "\nAnswer 1-C";
             }
 
-            if (((CheckBox) findViewById(R.id.answer2_1)).isChecked()) {
+            if (answer2_1.isChecked()) {
                 result = result + 0.5f;
+                answerResult += "\nThermodynamics";
             }
 
-            if (((CheckBox) findViewById(R.id.answer2_2)).isChecked()) {
+            if (answer2_2.isChecked()) {
                 result = result + 0.5f;
+                answerResult += "\nQuantum mechanics";
             }
 
-            if (((CheckBox) findViewById(R.id.answer2_3)).isChecked()) {
+            if (answer2_3.isChecked()) {
                 result = result - 0.5f;
+                answerResult += "\nScientology";
             }
 
-            if (((CheckBox) findViewById(R.id.answer2_4)).isChecked()) {
+            if (answer2_4.isChecked()) {
                 result = result - 0.5f;
+                answerResult += "\nAstronomy";
             }
 
-            if (((CheckBox) findViewById(R.id.answer3_1)).isChecked()) {
+            if (answer3_1.isChecked()) {
                 result = result - 0.5f;
+                answerResult += "\nChina";
             }
 
-            if (((CheckBox) findViewById(R.id.answer3_2)).isChecked()) {
+            if (answer3_2.isChecked()) {
                 result = result + 0.25f;
+                answerResult += "\nSpain";
             }
 
-            if (((CheckBox) findViewById(R.id.answer3_3)).isChecked()) {
+            if (answer3_3.isChecked()) {
                 result = result + 0.25f;
+                answerResult += "\nItaly";
             }
 
-            if (((CheckBox) findViewById(R.id.answer3_4)).isChecked()) {
+            if (answer3_4.isChecked()) {
                 result = result + 0.25f;
+                answerResult += "\nBelgium";
             }
 
-            if (((CheckBox) findViewById(R.id.answer3_5)).isChecked()) {
+            if (answer3_5.isChecked()) {
                 result = result + 0.25f;
+                answerResult += "\nGermany";
             }
 
-            EditText questionFour = (EditText) findViewById(R.id.answer4);
+            EditText questionFour = findViewById(R.id.answer4);
             String questionFourAnswer = questionFour.getText().toString();
-
-            if (questionFourAnswer.equalsIgnoreCase(Q5S)) {
+            if (questionFourAnswer.equalsIgnoreCase(Q4S)) {
                 result++;
+                answerResult += "\n" + questionFourAnswer;
             }
 
             answered = true;
@@ -92,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Context context = getApplicationContext();
-        CharSequence text = totalMark(result, name);
+        CharSequence text = totalMark(result, answerResult);
         int duration = Toast.LENGTH_LONG;
 
         Toast toast = Toast.makeText(context, text, duration);
@@ -104,21 +192,20 @@ public class MainActivity extends AppCompatActivity {
         answered = false;
     }
 
-
-    private String totalMark(float mark, String name) {
+    private String totalMark(float mark, String answer) {
 
         if (mark <= 2) {
-            return name + "\nYou fail the test.\nTry Again!";
+            return answer + "\nYou fail the test.\nTry Again!";
         }
 
         if (mark < 4) {
-            return name + "\nCongratulations!\nYou pass!";
+            return answer + "\nCongratulations!\nYou pass!";
         }
 
         if (mark == 4) {
-            return name + "\nYou´re amazing!\nYou pass with a 10!";
+            return answer + "\nYou´re amazing!\nYou pass with a 10!";
         }
-        return name;
+        return answer;
     }
 
 }
